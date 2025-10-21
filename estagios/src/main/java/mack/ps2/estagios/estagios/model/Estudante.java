2eaa8ccd2d1b64b6b5482b5b3da67f574e9a0133
Rikiments <142755990+Rikiments@users.noreply.github.com>
2025-10-21 23:41:44 +0000

    teste1

--- estagios/src/main/java/mack/ps2/estagios/estagios/model/Estudante.java
+++ estagios/src/main/java/mack/ps2/estagios/estagios/model/Estudante.java
@@ -1,9 +1,17 @@
 package mack.ps2.estagios.estagios.model;
 
 import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Estudante {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String nome;
     private String email;
     private LocalDate nascimento;
